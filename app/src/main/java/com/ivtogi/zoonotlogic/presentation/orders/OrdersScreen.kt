581d73c99a1c90a9a3cdd02a0fe839faea960f1e
Ivan Tortosa Gimeno <ivantorgim@gmail.com>
2024-05-24 17:53:17 +0200

    add first divider

--- app/src/main/java/com/ivtogi/zoonotlogic/presentation/orders/OrdersScreen.kt
+++ app/src/main/java/com/ivtogi/zoonotlogic/presentation/orders/OrdersScreen.kt
@@ -3,10 +3,12 @@ package com.ivtogi.zoonotlogic.presentation.orders
 import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Divider
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
@@ -47,6 +49,7 @@ fun OrdersScreen(
         ) { paddingValues ->
             Box(modifier = Modifier.padding(paddingValues)) {
                 LazyColumn {
+                    item { Divider(modifier = Modifier.fillMaxWidth()) }
                     items(state.orders) { order ->
                         OrderCard(
                             order = order,
