803eb99cd653a4b071d45f1aa3683a579b20230e
Ivan Tortosa Gimeno <ivantorgim@gmail.com>
2024-05-26 08:42:53 +0200

    delete default email & password

--- app/src/main/java/com/ivtogi/zoonotlogic/presentation/login/LoginState.kt
+++ app/src/main/java/com/ivtogi/zoonotlogic/presentation/login/LoginState.kt
@@ -2,9 +2,9 @@ package com.ivtogi.zoonotlogic.presentation.login
 
 data class LoginState(
     val userId: String? = null,
-    val email: String = "ivan_chella@hotmail.com",
+    val email: String = "",
     val emailError: Boolean = false,
-    val password: String = "ivantg",
+    val password: String = "",
     val passwordError: Boolean = false,
     val loginError: String? = null
 )
